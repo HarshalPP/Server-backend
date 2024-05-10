4985c00a0c954370fcd080846ff17a14ea3d3e4b
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-10 16:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ server.use(express.urlencoded({ limit: '500kb', extended: true }));
 server.use(bodyParser.urlencoded({ extended: true }));
 // server.use(cors(corsOptions));
 
-  setTimeout(connectDB(), 5000); // Retry after 5 seconds
+  connectDB() 
 
 // Showing Api is Running or not //
 
