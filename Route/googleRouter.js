aeeae88d93b4b61a049db8eacb3e2daae912bc7f
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-29 16:10:21 +0530

    Update googleRouter.js

--- Route/googleRouter.js
+++ Route/googleRouter.js
@@ -5,26 +5,12 @@ const passport = require('passport');
 
 // Google OAuth
 
-// Google OAuth callback route
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res, next) => {
-    // Call the next middleware to add token to response
-    next();
-}, (req, res) => {
-    res.redirect('http://www.authentichef.com/explore-dishes');
-});
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-// Middleware to add token to response
-router.use('/google/callback', (req, res) => {
-    // req.user contains the user object and the token from the strategy
-    const { token } = req.user;
-
-    // Send the token as part of the response
-    res.json({
-        success: true,
-        message: 'Google OAuth successful',
-        token: token
-    });
-});
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
+  res.redirect('http://www.authentichef.com/explore-dishes');
+}
+)
 
 router.get('/logout_google', (req, res) => {
   req.logout(err => {
