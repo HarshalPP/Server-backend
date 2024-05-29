122272c29b3e33e044c4b702688a1f894bda2701
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-29 18:25:44 +0530

    Update googleRouter.js

--- Route/googleRouter.js
+++ Route/googleRouter.js
@@ -12,20 +12,31 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 })
 );
 
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+
+// Google OAuth
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
-  const token = req.user ? req.user.activeToken : null;
-  if (token) {
-    // If user is authenticated and token is available, send token in API response
-    res.json({ success: true, message: 'Authentication successful', token });
-    // Redirect to explore dishes page and log the token
-    console.log("Token:", token);
-    res.redirect('http://www.authentichef.com/explore-dishes');
-  } else {
-    // If token is not available, redirect to explore dishes page
-    res.redirect('http://www.authentichef.com/explore-dishes');
-  }
+  res.redirect('http://www.authentichef.com/explore-dishes');
+}
+)
+
+router.get('/logout_google', (req, res) => {
+  req.logout(err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Logout failed' });
+    }
+    res.redirect('http://www.authentichef.com/');
+  });
 });
 
+module.exports = router;
 
 router.get('/logout_google', (req, res) => {
   req.logout(err => {
