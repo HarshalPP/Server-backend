983e52a2383924e4b59252ae64967693134bd314
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-30 11:47:03 +0530

    Update googleRouter.js

--- Route/googleRouter.js
+++ Route/googleRouter.js
@@ -8,35 +8,35 @@ const User = require("../Model/User");
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-// router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
-//     // Assuming req.user has the authenticated user information including the token
-//     const token = req.user ? req.user.activeToken : null;
-//     // Redirect to explore dishes page
-//     res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
-// });
-
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
     // Assuming req.user has the authenticated user information including the token
     const token = req.user ? req.user.activeToken : null;
-    if (token) {
-        // Redirect with token in query parameter
-        res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
-        // Send JSON response with the token
-        res.json({
-            success: true,
-            message: 'Authentication successful',
-            token: token
-        });
-    } else {
-        // Redirect to failure URL with error message
-        res.redirect('http://www.authentichef.com/?error=Authentication+failed');
-        // Send JSON response indicating failure
-        res.status(401).json({
-            success: false,
-            message: 'Authentication failed'
-        });
-    }
+    // Redirect to explore dishes page
+    res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
 });
+
+// router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
+//     // Assuming req.user has the authenticated user information including the token
+//     const token = req.user ? req.user.activeToken : null;
+//     if (token) {
+//         // Redirect with token in query parameter
+//         res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
+//         // Send JSON response with the token
+//         res.json({
+//             success: true,
+//             message: 'Authentication successful',
+//             token: token
+//         });
+//     } else {
+//         // Redirect to failure URL with error message
+//         res.redirect('http://www.authentichef.com/?error=Authentication+failed');
+//         // Send JSON response indicating failure
+//         res.status(401).json({
+//             success: false,
+//             message: 'Authentication failed'
+//         });
+//     }
+// });
 
 router.get('/logout_google', (req, res) => {
     req.logout(err => {
