d40d8d53f05888445a09ff23e88264f9c702655d
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-31 11:34:33 +0530

    Update googleRouter.js

--- Route/googleRouter.js
+++ Route/googleRouter.js
@@ -1,67 +1,145 @@
-const express = require('express');
-const router = express.Router();
 const passport = require('passport');
-const { generateToken } = require("../config/jwtToken");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require("../Model/User");
+const { generateToken } = require("../config/jwtToken");
+const sendEmail = require("../Utils/SendEmail");
+const dotenv = require('dotenv');
+dotenv.config();
 
-// Google OAuth
+module.exports = function (passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: 'https://server-backend-gamma.vercel.app/Google_OAuth/google/callback',
+    }, async (accessToken, refreshToken, profile, done) => {
+        try {
+            const newUser = {
+                googleId: profile.id,
+                firstname: profile.name.givenName,
+                lastname: profile.name.familyName,
+                email: profile.emails[0].value,
+                img: profile.photos[0].value,
+            };
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+            let user = await User.findOne({ googleId: profile.id });
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
-    // Assuming req.user has the authenticated user information including the token
-    const token = req.user ? req.user.activeToken : null;
-    // Redirect to explore dishes page
-    res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
-});
+            if (!user) {
+                user = await User.findOne({ email: profile.emails[0].value });
+                if (!user) {
+                    console.log("create user");
+                    user = await User.create(newUser);
+                } else {
+                    console.log("Login with Google");
+                    user.googleId = profile.id;
+                    await user.save();
+                }
+            }
 
-// router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
-//     // Assuming req.user has the authenticated user information including the token
-//     const token = req.user ? req.user.activeToken : null;
+            const token = generateToken({ id: user._id });
 
-//     // Redirect to explore dishes page with token
-//     res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
+            user.activeToken = token;
+            console.log(token);
+            await user.save();
+            await sendEmail({
+                to: user.email,
+                subject: 'Google Login Successful',
+                text: `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f4;
+                            margin: 0;
+                            padding: 0;
+                            color: #333;
+                        }
+                        .container {
+                            width: 100%;
+                            max-width: 600px;
+                            margin: 0 auto;
+                            background-color: #ffffff;
+                            border-radius: 8px;
+                            overflow: hidden;
+                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                        }
+                        .header {
+                            background-color: #4CAF50;
+                            color: #ffffff;
+                            padding: 20px;
+                            text-align: center;
+                        }
+                        .header h1 {
+                            margin: 0;
+                            font-size: 24px;
+                        }
+                        .content {
+                            padding: 20px;
+                        }
+                        .content p {
+                            font-size: 16px;
+                            line-height: 1.6;
+                        }
+                        .content a {
+                            display: inline-block;
+                            background-color: #4CAF50;
+                            color: #ffffff;
+                            padding: 10px 20px;
+                            text-decoration: none;
+                            border-radius: 4px;
+                            margin-top: 20px;
+                        }
+                        .footer {
+                            background-color: #f4f4f4;
+                            color: #777;
+                            text-align: center;
+                            padding: 10px;
+                            font-size: 12px;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="container">
+                        <div class="header">
+                            <h1>Google OAuth Successful</h1>
+                        </div>
+                        <div class="content">
+                            <p>Hi ${user.firstname},</p>
+                            <p>You have successfully logged in using Google Login.</p>
+                            <p>Click on the link below to continue:</p>
+                            <a href="http://www.authentichef.com/explore-dishes">Click Here</a>
+                        </div>
+                        <div class="footer">
+                            <p>If you did not initiate this request, please ignore this email.</p>
+                        </div>
+                    </div>
+                </body>
+                </html>
+                `
+            });
 
-//     // Send JSON response with the token
-//     res.json({
-//         success: true,
-//         message: 'Authentication successful',
-//         token: token
-//     });
-// });
-
-
-// router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://www.authentichef.com/' }), (req, res) => {
-//     // Assuming req.user has the authenticated user information including the token
-//     const token = req.user ? req.user.activeToken : null;
-//     if (token) {
-//         // Redirect with token in query parameter
-//         res.redirect(`http://www.authentichef.com/explore-dishes?token=${token}`);
-//         // Send JSON response with the token
-//         res.json({
-//             success: true,
-//             message: 'Authentication successful',
-//             token: token
-//         });
-//     } else {
-//         // Redirect to failure URL with error message
-//         res.redirect('http://www.authentichef.com/?error=Authentication+failed');
-//         // Send JSON response indicating failure
-//         res.status(401).json({
-//             success: false,
-//             message: 'Authentication failed'
-//         });
-//     }
-// });
-
-router.get('/logout_google', (req, res) => {
-    req.logout(err => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ message: 'Logout failed' });
+            done(null, user);
+        } catch (error) {
+            console.error(error);
+            done(error, null);
         }
-        res.redirect('http://www.authentichef.com/');
-    });
-});
+    }));
 
-module.exports = router;
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id)
+            .then(user => {
+                done(null, user);
+            })
+            .catch(err => {
+                console.error(err);
+                done(err, null);
+            });
+    });
+};
