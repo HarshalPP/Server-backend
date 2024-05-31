f9e3dc7780a12cc4515f31f4713bd59ea4c14dbd
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-05-31 11:16:52 +0530

    Update googleconfig.js

--- config/googleconfig.js
+++ config/googleconfig.js
@@ -130,7 +130,7 @@ module.exports = function (passport) {
             });
             
 
-            done({ user:user,token:token});
+            done(user,token);
 
             
         } catch (error) {
