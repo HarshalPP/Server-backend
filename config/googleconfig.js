7684a71aade1fe41e7030220306cde7c9c6d3b88
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-06-19 00:27:37 +0530

    Update googleconfig.js

--- config/googleconfig.js
+++ config/googleconfig.js
@@ -41,101 +41,104 @@ module.exports = function (passport) {
             console.log(token);
             await user.save();
              // Send email after a delay
-            // Send email after a delay
-            setTimeout(async () => {
-                await sendEmail({
-                    to: user.email,
-                    subject: 'Google Login Successful',
-                    text: `
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <style>
-                                body {
-                                    font-family: Arial, sans-serif;
-                                    background-color: #f4f4f4;
-                                    margin: 0;
-                                    padding: 0;
-                                    color: #333;
-                                }
-                                .container {
-                                    width: 100%;
-                                    max-width: 600px;
-                                    margin: 0 auto;
-                                    background-color: #ffffff;
-                                    border-radius: 8px;
-                                    overflow: hidden;
-                                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                                }
-                                .header {
-                                    background-color: #F38181;
-                                    color: #ffffff;
-                                    padding: 20px;
-                                    text-align: center;
-                                }
-                                .header h1 {
-                                    margin: 0;
-                                    font-size: 24px;
-                                }
-                                .content {
-                                    padding: 20px;
-                                }
-                                .content p {
-                                    font-size: 16px;
-                                    line-height: 1.6;
-                                }
-                                .content a {
-                                    display: inline-block;
-                                    background-color: #4CAF50;
-                                    color: #ffffff;
-                                    padding: 10px 20px;
-                                    text-decoration: none;
-                                    border-radius: 4px;
-                                    margin-top: 20px;
-                                }
-                                .footer {
-                                    background-color: #f4f4f4;
-                                    color: #777;
-                                    text-align: center;
-                                    padding: 10px;
-                                    font-size: 12px;
-                                }
-                                .head {
-                                    text-align: center;
-                                }
-                                .img {
-                                    max-width: 150px;
-                                }
-                                .imgs {
-                                    display: flex;
-                                    justify-content: center;
-                                }
-                            </style>
-                        </head>
-                        <body>
-                            <div class="container">
-                                <div class="header">
-                                    <div class="imgs">
-                                        <img class="img" src="https://authimages.s3.eu-west-2.amazonaws.com/logo.png" alt="authentichef">
+          setTimeout(async () => {
+                try {
+                    await sendEmail({
+                        to: user.email,
+                        subject: 'Google Login Successful',
+                        text: `
+                            <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <style>
+                                    body {
+                                        font-family: Arial, sans-serif;
+                                        background-color: #f4f4f4;
+                                        margin: 0;
+                                        padding: 0;
+                                        color: #333;
+                                    }
+                                    .container {
+                                        width: 100%;
+                                        max-width: 600px;
+                                        margin: 0 auto;
+                                        background-color: #ffffff;
+                                        border-radius: 8px;
+                                        overflow: hidden;
+                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                    }
+                                    .header {
+                                        background-color: #F38181;
+                                        color: #ffffff;
+                                        padding: 20px;
+                                        text-align: center;
+                                    }
+                                    .header h1 {
+                                        margin: 0;
+                                        font-size: 24px;
+                                    }
+                                    .content {
+                                        padding: 20px;
+                                    }
+                                    .content p {
+                                        font-size: 16px;
+                                        line-height: 1.6;
+                                    }
+                                    .content a {
+                                        display: inline-block;
+                                        background-color: #4CAF50;
+                                        color: #ffffff;
+                                        padding: 10px 20px;
+                                        text-decoration: none;
+                                        border-radius: 4px;
+                                        margin-top: 20px;
+                                    }
+                                    .footer {
+                                        background-color: #f4f4f4;
+                                        color: #777;
+                                        text-align: center;
+                                        padding: 10px;
+                                        font-size: 12px;
+                                    }
+                                    .head {
+                                        text-align: center;
+                                    }
+                                    .img {
+                                        max-width: 150px;
+                                    }
+                                    .imgs {
+                                        display: flex;
+                                        justify-content: center;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                                <div class="container">
+                                    <div class="header">
+                                        <div class="imgs">
+                                            <img class="img" src="https://authimages.s3.eu-west-2.amazonaws.com/logo.png" alt="authentichef">
+                                        </div>
+                                    </div>
+                                    <div class="head">
+                                        <h1>Welcome To Authentichef</h1>
+                                    </div>
+                                    <div class="content">
+                                        <p>Hi ${user.firstname},</p>
+                                        <p>You have successfully logged in using Google Login.</p>
+                                    </div>
+                                    <div class="footer">
+                                        <p>If you did not initiate this request, please ignore this email.</p>
                                     </div>
                                 </div>
-                                <div class="head">
-                                    <h1>Welcome To Authentichef</h1>
-                                </div>
-                                <div class="content">
-                                    <p>Hi ${user.firstname},</p>
-                                    <p>You have successfully logged in using Google Login.</p>
-                                </div>
-                                <div class="footer">
-                                    <p>If you did not initiate this request, please ignore this email.</p>
-                                </div>
-                            </div>
-                        </body>
-                        </html>
-                    `,
-                });
+                            </body>
+                            </html>
+                        `,
+                    });
+                } catch (emailError) {
+                    console.error('Error sending email:', emailError);
+                }
             }, 5000); // Delay email by 5000 milliseconds (5 seconds)
             done(null, user);
         } catch (error) {
