012544f784acec1bb1d1c57dd3268af37872c4b0
HarshalPP <130646816+HarshalPP@users.noreply.github.com>
2024-06-20 12:38:00 +0530

    Update db.js

--- Utils/db.js
+++ Utils/db.js
@@ -1,17 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds (adjust as needed)
-        });
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.error("MongoDB connection error:", err.message);
-        process.exit(1); // Exit process with failure
-    }
+  await mongoose.connect(process.env.MONGO_URI, {});
+  console.log("MongoDB Connected");
 };
 
 module.exports = connectDB;
